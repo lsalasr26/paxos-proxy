52774779d95d090fa12eec886bb831421b18d011
Luis Salas <lsalasr@ucenfotec.ac.cr>
2024-07-25 15:44:23 -0600

    first commit

---
+++ server.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/proxy/:symbol', async (req, res) => {
+    const symbol = req.params.symbol;
+    try {
+        const response = await fetch(`https://api.paxos.com/v2/markets/${symbol}/ticker`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).send('Error al obtener los datos');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor proxy corriendo en el puerto ${PORT}`);
+});
