dc9e27fffc0a951f7c446dcaf705f9b800cf68a6
Luis Salas <lsalasr@ucenfotec.ac.cr>
2024-08-03 16:33:28 -0600

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://566e98hs.hs-sites.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
